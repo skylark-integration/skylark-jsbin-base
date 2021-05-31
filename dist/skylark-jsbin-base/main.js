72ec545965515df76e8eec2f662d282f604485c5
hudaokeji <github@hudaokeji.com>
2021-05-31 11:16:08 +0900

    init commit

---
+++ dist/skylark-jsbin-base/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-jsbin-base - A version of jsbin-client  that ported to running on skylarkjs.
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-jsbin-base/
+ * @license MIT
+ */
+define(["./jsbin"],function(n){return n});
+//# sourceMappingURL=sourcemaps/main.js.map
